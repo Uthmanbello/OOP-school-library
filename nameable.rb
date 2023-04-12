93378713ecc40a337ac8805c4e60c7c761b8d5f3
Uthmanbello <uthmanbello88@gmail.com>
2023-04-12 15:36:35 +0100

    Add nameable.rb

---
+++ nameable.rb
@@ -0,0 +1,29 @@
+class Nameable
+  def correct_name
+    raise NotImplementedError
+  end
+end
+
+class Decorator < Nameable
+  attr_accessor :nameable
+  
+  def initialize(nameable)
+    @nameable = nameable
+  end
+  
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    "#{@nameable.correct_name}.capitalize()"
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    "#{@nameable.correct_name}.truncate(10)"
+  end
+end
--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require "./nameable.rb"
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', _parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
@@ -12,9 +14,20 @@ class Person
     of_age? || parent_permission ? true : false
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+puts capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+puts capitalizedTrimmedPerson.correct_name
