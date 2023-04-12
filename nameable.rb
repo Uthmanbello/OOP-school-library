369ec69f2778866267c04d7b32071664dcb3a368
Uthmanbello <uthmanbello88@gmail.com>
2023-04-12 15:44:14 +0100

    Fix errors using linters

--- nameable.rb
+++ nameable.rb
@@ -6,11 +6,12 @@ end
 
 class Decorator < Nameable
   attr_accessor :nameable
-  
+
   def initialize(nameable)
+    super()
     @nameable = nameable
   end
-  
+
   def correct_name
     @nameable.correct_name
   end
--- person.rb
+++ person.rb
@@ -1,10 +1,11 @@
-require "./nameable.rb"
+require './nameable'
 
 class Person < Nameable
   def initialize(age, name = 'Unknown', _parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    super()
   end
 
   attr_accessor :name, :age
@@ -27,7 +28,7 @@ end
 
 person = Person.new(22, 'maximilianus')
 puts person.correct_name
-capitalizedPerson = CapitalizeDecorator.new(person)
-puts capitalizedPerson.correct_name
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-puts capitalizedTrimmedPerson.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
