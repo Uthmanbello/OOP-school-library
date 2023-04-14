c127068a0dd723d24e7a13e27eb12b4394128185
Uthmanbello <uthmanbello88@gmail.com>
2023-04-14 20:57:25 +0100

    Fix errors using linters

--- main.rb
+++ main.rb
@@ -2,41 +2,46 @@
 require 'date'
 require './app'
 
+def display_menu
+  puts "\nWelcome to School Library App!"
+  puts "\nPlease choose an option by entering a number:"
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+end
+
+def process_option(option, app)
+  menu = {
+    1 => :list_books,
+    2 => :list_people,
+    3 => :create_person,
+    4 => :create_book,
+    5 => :create_rental,
+    6 => :list_rentals_for_person,
+    7 => :exit
+  }
+
+  if menu.key?(option)
+    app.send(menu[option])
+    return true if menu[option] == :exit
+  else
+    puts 'Invalid option.'
+  end
+
+  false
+end
+
 def main
   app = App.new
 
   loop do
-    puts "\nWelcome to School Library App!"
-    puts "\nPlease choose an option by entering a number:"
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-
+    display_menu
     option = gets.chomp.to_i
-
-    case option
-    when 1
-      app.list_books
-    when 2
-      app.list_people
-    when 3
-      app.create_person
-    when 4
-      app.create_book
-    when 5
-      app.create_rental
-    when 6
-      app.list_rentals_for_person
-    when 7
-      puts 'Thank you for using this app!'
-      break
-    else
-      puts 'Invalid option.'
-    end
+    break if process_option(option, app)
   end
 end
 
