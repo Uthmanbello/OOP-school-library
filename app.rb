e9cfee1df1422775dafdf19a1e7e56a31509d9f7
Uthmanbello <uthmanbello88@gmail.com>
2023-04-14 19:25:14 +0100

    Use methods in main.rb

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ require './teacher'
 
 class App
   attr_accessor :books, :rentals, :people, :student, :teacher
+
   def initialize
     @books = []
     @people = []
@@ -25,33 +26,72 @@ class App
     end
   end
 
-  def create_person(name, age, type, specialization = nil)
-    if type == 1
+  def create_person
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
+    person_type = gets.chomp.to_i
+    if person_type == 1
+      puts 'Age:'
+      age = gets.chomp
+      puts 'Name:'
+      name = gets.chomp
+      puts 'Has Parent Permission? [Y/N]:'
+      parent_permission = gets.chomp.downcase == 'y'
       person = Student.new(classroom = nil, age, name)
-    elsif type == 2
+    elsif person_type == 2
+      puts 'Age:'
+      age = gets.chomp
+      puts 'Name:'
+      name = gets.chomp
+      puts 'Specialization:'
+      specialization = gets.chomp
       person = Teacher.new(age, name, specialization)
     else
-      raise "Invalid person type"
-    end 
+      puts 'Invalid input.'
+    end
+
     @people.push(person)
     person
+    puts 'Person created successfully'
   end
 
-  def create_book(title, author)
+  def create_book
+    puts 'Title:'
+    title = gets.chomp
+    puts 'Author:'
+    author = gets.chomp
     book = Book.new(title, author)
     @books.push(book)
     book
+    puts 'Book created successfully'
   end
 
-  def create_rental(book, person, date)
-    rental = Rental.new(date, book, person)
+  def create_rental
+    puts 'Select a book from the following list by number:'
+      @books.each_with_index do |book, index|
+        puts "#{index}) Title: #{book.title}, Author: #{book.author}"
+      end
+      book = gets.chomp.to_i
+
+    puts 'Select a person from the following list by number (not id):'
+      @people.each_with_index do |person, index|
+        puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      end
+      person = gets.chomp.to_i
+
+    puts 'Date (YYYY-MM-DD):'
+      date = Date.parse(gets.chomp)
+      
+    rental = Rental.new(date, @books[book], @people[person])
     @rentals.push(rental)
     rental
+    puts 'Rental created successfully'
   end
 
-  def list_rentals_for_person(person_id)
+  def list_rentals_for_person
+    puts 'ID of person:'
+    person_id = gets.chomp.to_i
     rentals = @rentals.select { |rental| rental.person.id == person_id }
-    puts "Rentals:"
+    puts 'Rentals:'
     rentals.each do |rental|
       puts "Date: #{rental.date}, Book: '#{rental.book.title}' by #{rental.book.author}"
     end
--- main.rb
+++ main.rb
@@ -3,18 +3,18 @@ require 'date'
 require './app'
 
 def main
-  app = App.new()
+  app = App.new
 
   loop do
     puts "\nWelcome to School Library App!"
     puts "\nPlease choose an option by entering a number:"
-    puts "1 - List all books"
-    puts "2 - List all people"
-    puts "3 - Create a person"
-    puts "4 - Create a book"
-    puts "5 - Create a rental"
-    puts "6 - List all rentals for a given person id"
-    puts "7 - Exit"
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
 
     option = gets.chomp.to_i
 
@@ -24,60 +24,18 @@ def main
     when 2
       app.list_people
     when 3
-      puts "Do you want to create a student (1) or a teacher (2)? [Input the number]:"
-      person_type = gets.chomp.to_i
-      if person_type == 1
-        puts "Age:"
-        age = gets.chomp
-        puts "Name:"
-        name = gets.chomp
-        puts "Has Parent Permission? [Y/N]:"
-        parent_permission = gets.chomp.downcase == 'y'
-      elsif person_type == 2
-        puts "Age:"
-        age = gets.chomp
-        puts "Name:"
-        name = gets.chomp
-        puts "Specialization:"
-        specialization = gets.chomp
-      else
-        puts "Invalid input."
-      end
-      app.create_person(name, age, person_type) 
-      puts "Person created successfully" 
+      app.create_person
     when 4
-      puts "Title:"
-      title = gets.chomp
-      puts "Author:"
-      author = gets.chomp
-      app.create_book(title, author)
-      puts "Book created successfully"
+      app.create_book
     when 5
-      puts "Select a book from the following list by number:"
-      app.books.each_with_index do |book, index|
-        puts "#{index}) Title: #{book.title}, Author: #{book.author}"
-      end
-      book = gets.chomp.to_i
-
-      puts "Select a person from the following list by number (not id):"
-      app.people.each_with_index do |person, index|
-        puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-      end
-      person = gets.chomp.to_i
-
-      puts "Date (YYYY-MM-DD):"
-      date = Date.parse(gets.chomp)
-      app.create_rental(app.books[book], app.people[person], date)
-      puts "Rental created successfully"
+      app.create_rental
     when 6
-      puts "ID of person:"
-      person_id = gets.chomp.to_i
-      app.list_rentals_for_person(person_id)
+      app.list_rentals_for_person
     when 7
-      puts "Thank you for using this app!"
+      puts 'Thank you for using this app!'
       break
     else
-      puts "Invalid option."
+      puts 'Invalid option.'
     end
   end
 end
